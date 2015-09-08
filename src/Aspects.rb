c84581b3e20e4d87ed423748cd18bac588a94d13
anicolasgar <a.nicolasgar@gmail.com>
2015-09-08 20:58:52 -0300

    merge

--- src/Aspects.rb
+++ src/Aspects.rb
@@ -1,14 +1,17 @@
 class Aspects
 
-  def self.on(*arg)
-    aspects_encontrados = find_aspects(*args)
+  def self.on(arg, *args)
+    aspects_encontrados=0
+   # aspects_encontrados = find_aspects(arg, *args)
 
-    throw(ArgumentError,"Origen Vacio") if aspects_encontrados.size==0
+
+    raise ArgumentError, " wrong number of arguments (0 for +1)" if aspects_encontrados==0
+    raise ArgumentError, "Origen Vacio" if aspects_encontrados==0
 
   end
 
-  def find_aspects(*args)
-  return 1
+  def self.find_aspects(*args)
+  return 0
   end
 
 end
