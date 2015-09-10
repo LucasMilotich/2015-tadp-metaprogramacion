adbf7c458a53ecb81184599e6f638f6fe0d625fd
emiiSierra <sierraemii@gmail.com>
2015-09-10 19:01:18 -0300

    modifico todos las condiciones y las coloque como mixins, cosa que cuando se llame siempre a la clase Condicion

--- spec/condiciones_spec.rb
+++ spec/condiciones_spec.rb
@@ -21,35 +21,33 @@ describe 'test condiciones' do
   metodos_publ = A.public_instance_methods(false).map {|m| A.new.method(m)}
   metodos_priv = A.private_instance_methods(false).map {|m| A.new.method(m)}
 
+  foo = Condicion.new(metodos_publ.first)
+  bar = Condicion.new(metodos_publ.last)
+  prfoo = Condicion.new(metodos_priv.first)
+
   #Selector
   it 'metodo publico matchea regex' do
-    foo = Selector.new(metodos_publ.first)
     expect(foo.name(/foo/)).to be_truthy
   end
 
   it 'metodo privado matchea regex' do
-    foo = Selector.new(metodos_priv.first)
-    expect(foo.name(/fo{2}/)).to be_truthy
+    expect(prfoo.name(/fo{2}/)).to be_truthy
   end
 
   it 'metodo publico no matchea regex' do
-    bar = Selector.new(metodos_publ.last)
     expect(bar.name(/fo{2}/)).to be_falsey
   end
 
   #Visibilidad
   it 'metodo publico es publico' do
-    foo = Visibilidad.new(metodos_publ.first)
     expect(foo.is_public).to be_truthy
   end
 
   it 'metodo privado es privado' do
-    foo = Visibilidad.new(metodos_priv.first)
-    expect(foo.is_private).to be_truthy
+    expect(prfoo.is_private).to be_truthy
   end
 
   it 'metodo privado no es publico' do
-    prfoo = Visibilidad.new(metodos_priv.first)
     expect(prfoo.is_public).to be_falsey
   end
 
@@ -58,66 +56,69 @@ describe 'test condiciones' do
   optional = proc {|parametro| parametro.first == :opt}
 
   it 'metodo de 5 parametros matchea' do
-    foo = CantidadParametros.new(metodos_publ.first)
     expect(foo.has_parameters(5)).to be_truthy
   end
 
   it 'metodo de 5 parametros no matchea con 2' do
-    foo = CantidadParametros.new(metodos_publ.first)
     expect(foo.has_parameters(2)).to be_falsey
   end
 
   it 'metodo de 2 parametros obligatorios matchea' do
-    foo = CantidadParametros.new(metodos_publ.first)
     expect(foo.has_parameters(2,mandatory)).to be_truthy
   end
 
   it 'metodo de 3 parametros opcionales matchea' do
-    foo = CantidadParametros.new(metodos_publ.first)
     expect(foo.has_parameters(3,optional)).to be_truthy
   end
 
   it 'metodo privado de 1 parametro obligatorio matchea' do
-    foo = CantidadParametros.new(metodos_priv.first)
-    expect(foo.has_parameters(1,mandatory)).to be_truthy
+    expect(bar.has_parameters(1,mandatory)).to be_truthy
   end
 
   it 'metodo con menos parametros opcionales no matchea' do
-    foo = CantidadParametros.new(metodos_publ.last)
-    expect(foo.has_parameters(2,optional)).to be_falsey
+    expect(bar.has_parameters(2,optional)).to be_falsey
   end
 
   #Nombre parametros
   it 'metodo de 2 parametros matchea regex' do
-    foo = NombreParametros.new(metodos_publ.last)
-    expect(foo.has_parameters(2,/p.*/)).to be_truthy
+    expect(bar.has_parameters(2,/p.*/)).to be_truthy
   end
 
   it 'metodo no matchea por parametros de mas en regex' do
-    foo = NombreParametros.new(metodos_publ.first)
     expect(foo.has_parameters(2,/p.*/)).to be_falsey
   end
 
   it 'metodo no matchea parametros con regex' do
-    foo = NombreParametros.new(metodos_publ.last)
-    expect(foo.has_parameters(1,/f.*/)).to be_falsey
+    expect(bar.has_parameters(1,/f.*/)).to be_falsey
   end
 
   #Neg
+  #Se extiende a la clase Condicion para que las funciones llamadas dentro de neg estén dentro de su contexto.
+  module Funciones_para_negar
+    def negar_private
+        self.neg(is_private)
+    end
+    def negar_varios
+       self.neg(is_private,has_parameters(1),name(/j.*/))
+    end
+    def negar_falso
+      self.neg(is_private,has_parameters(2))
+    end
+  end
 
   it 'metodo niega 1 condicion y matchea' do
-    foo = NegCondicion.new(metodos_publ.last)
-    expect(foo.neg(is_private)).to be_truthy
+    bar.extend Funciones_para_negar
+    expect(bar.negar_private).to be_truthy
   end
 
   it 'metodo niega todas las condiciones (>1) y matchea' do
-    foo = NegCondicion.new(metodos_publ.last)
-    expect(foo.neg(is_private,has_parameters(1),name(/j.*/))).to be_truthy
+    bar.extend Funciones_para_negar
+    expect(bar.negar_varios).to be_truthy
   end
 
   it 'metodo no niega una condicion y no matchea' do
-    foo = NegCondicion.new(metodos_publ.last)
-    expect(foo.neg(is_private,has_parameters(2))).to be_falsey
+    bar.extend Funciones_para_negar
+    expect(bar.negar_falso).to be_falsey
   end
 
 end
--- src/condicion.rb
+++ src/condicion.rb
@@ -1,18 +1,6 @@
-class Condicion
-
-  attr_accessor :metodo
-
-  def self.new(m)
-    a = super()
-    a.metodo = m
-    a
-  end
-
-end
-
 module CondicionRegex
   def cumple_regex?(objeto, regex)
-    regex.match(objeto).class == MatchData
+    regex.match(objeto).is_a?(MatchData)
   end
 end
 
@@ -22,21 +10,7 @@ module CondicionParametros
   end
 end
 
-class CantidadParametros < Condicion
-  include CondicionParametros
-
-  mandatory = proc {|parametro| parametro.first == :req}
-  optional = proc {|parametro| parametro.first == :opt}
-
-  def has_parameters(cantidad, tipo = proc { |p| p })
-    cumplen_condicion?(tipo,cantidad)
-
-  end
-
-end
-
-class Visibilidad < Condicion
-
+module Visibilidad
   def cumple_visibilidad?(visibilidad)
     @metodo.owner.method(visibilidad).call(false).include?(@metodo.name)
   end
@@ -51,24 +25,64 @@ class Visibilidad < Condicion
 
 end
 
-class Selector < Condicion
+module CantidadParametros
+  include CondicionParametros
+
+  mandatory = proc {|parametro| parametro.first == :req}
+  optional = proc {|parametro| parametro.first == :opt}
+
+  def has_parameters(cantidad, tipo = proc { |p| p })
+    cumplen_condicion?(tipo,cantidad)
+
+  end
+
+end
+
+module Selector
   include CondicionRegex
   def name(regex)
     cumple_regex?(@metodo.name,regex)
   end
 end
 
-class NombreParametros < Condicion
-    include CondicionRegex
-    include CondicionParametros
-    def has_parameters(cantidad, regex)
-        cumplen_condicion?( proc {|parametro| cumple_regex?(parametro.last,regex)},cantidad)
-    end
+module NombreParametros
+  include CondicionRegex
+  include CondicionParametros
+  def has_parameters(cantidad, regex)
+    cumplen_condicion?( proc {|parametro| cumple_regex?(parametro.last,regex)},cantidad)
+  end
 end
 
-class NegCondicion < Condicion
-  def neg (condicion, *condiciones)
-    condicion.concat(condiciones).all? {|ci| self.ci == false}
+module NegCondicion
 
+  def neg (*condiciones)
+    condiciones.all? {|ci| ci == false}
   end
+
+end
+
+class Condicion
+  include Visibilidad
+  include NombreParametros
+  alias_method :has_parameters_n, :has_parameters
+  include CantidadParametros
+  include Selector
+  include NegCondicion
+
+  attr_accessor :metodo
+
+  def self.new(m)
+    a = super()
+    a.metodo = m
+    a
+  end
+
+  def has_parameters(cantidad, arg = proc { |p| p } )
+    if arg.is_a?(Regexp) then
+      has_parameters_n(cantidad, arg)
+    else
+      super(cantidad,arg)
+    end
+  end
+
 end
